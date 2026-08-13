8c2d8ca4d15f104adcbad097dd3eb5e034d34cb6
TechnicalShree <krushnasr96@gmail.com>
2026-08-13 22:24:06 +0530

    feat: Prevent browser from auto-filling connection credentials in Doc Sync Settings form

--- data_sync/data_sync/doctype/doc_sync_settings/doc_sync_settings.js
+++ data_sync/data_sync/doctype/doc_sync_settings/doc_sync_settings.js
@@ -3,6 +3,23 @@
 
 frappe.ui.form.on("Doc Sync Settings", {
 	refresh(frm) {
+		// This form contains connection credentials. Prevent the browser from
+		// inserting previously saved values into the settings fields.
+		[
+			"site_identifier",
+			"target_url",
+			"api_key",
+			"api_secret",
+			"max_retries",
+			"request_timeout",
+			"batch_size",
+		].forEach((fieldname) => {
+			const field = frm.fields_dict[fieldname];
+			if (field && field.$input) {
+				field.$input.attr("autocomplete", fieldname === "api_secret" ? "new-password" : "off");
+			}
+		});
+
 		frm.add_custom_button(__("Test Connection"), () => {
 			frappe.call({
 				method: "data_sync.sync.test_connection",
