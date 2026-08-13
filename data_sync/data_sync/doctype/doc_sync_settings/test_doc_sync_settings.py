099d41f2963521039726df2dc9754835c085a492
“Vikas <Vikas.moin777@gmail.com>
2026-08-13 21:47:37 +0530

    feat: Add Doc Sync Queue and Doc Sync Settings doctype with associated files

---
+++ data_sync/data_sync/doctype/doc_sync_settings/test_doc_sync_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, vikas moin and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestDocSyncSettings(FrappeTestCase):
+	pass
